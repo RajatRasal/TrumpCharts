444a30ab17502f4285b62f0af36cde9df7f00a9a
Rajat Rasal <rrr2417@ic.ac.uk>
2018-12-08 21:53:26 +0000

    Word2Vec helper function using lists. RAJAT

---
+++ tests/word2vec_helper.py
@@ -0,0 +1,31 @@
+import re
+import pickle
+import numpy as np
+
+tweets = []
+
+with open("../data/tweets_edited.pkl", "rb") as f:
+    unpickler = pickle.Unpickler(f)
+    sentences = unpickler.load()
+
+    wordlist = None
+
+    with open("../data/tweets_wordlist.pkl", "rb") as g:
+        # unpickler = pickle.Unpickler(g)
+        wordlist = pickle.load(g)
+    
+    sizes_sum = 0
+    
+    for i in range(len(sentences)):
+        s_lower_split = re.split("( |[^a-zA-Z\d])", "".join(sentences[i]).lower())
+        s_lower_split = list(filter(lambda x : x not in [" ", ""], s_lower_split))
+        s_lower_split = list(map(lambda x : wordlist.index(x), s_lower_split))
+        sizes_sum += len(s_lower_split)
+        tweets.append(s_lower_split)
+
+        if i % 1000 == 0:
+            print(i)
+
+tweets = np.asmatrix(tweets, dtype=np.int32)
+
+np.save("../data/tweets_integerized.npy", tweets)
